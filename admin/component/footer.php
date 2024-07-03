6e19d2d470d55e120963444e10f131749ad5ef29
tuannguyen-1309 <tuandungyn2@gmail.com>
2024-07-03 15:59:39 +0700

    component admin

---
+++ admin/component/footer.php
@@ -0,0 +1,7 @@
+    <!-- Start footer -->
+    <footer class="page-footer">
+      <p class="mb-0">Copyright © 2024. All right reserved.</p>
+    </footer>
+    <!-- End footer -->
+  </body>
+</html>
---
+++ admin/component/sidebar.php
@@ -0,0 +1,89 @@
+<aside class="sidebar-wrapper">
+      <div class="simplebar-wrapper">
+        <div class="simplebar-mask">
+          <div class="simplebar-offset">
+            <div class="simplebar-content-wrapper">
+              <div class="simplebar-content" style="padding: 0px">
+                <div class="sidebar-header">
+                  <div class="logo-icon">
+                    <img src="./images/logo.png" alt="" />
+                  </div>
+                </div>
+                <div class="sidebar-nav">
+                  <!-- Start navigation -->
+                  <ul class="metismenu">
+                    <li>
+                      <a href="index.php" class="has-arrow">
+                        <div class="parent-icon">
+                          <i class="bi bi-house"></i>
+                        </div>
+                        <div class="menu-title">Trang chủ</div>
+                      </a>
+                    </li>
+                    <li class="menu-label">Quản lý</li>
+                    <li>
+                      <a href="index.php?act=adddm" class="has-arrow">
+                        <div class="child-icon">
+                          <i class="bi bi-caret-right"></i>
+                        </div>
+                        <div class="menu-title">Danh mục</div>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="index.php?act=addsp" class="has-arrow">
+                        <div class="child-icon">
+                          <i class="bi bi-caret-right"></i>
+                        </div>
+                        <div class="menu-title">Sản phẩm</div>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="index.php?act=listgiohang" class="has-arrow">
+                        <div class="child-icon">
+                          <i class="bi bi-caret-right"></i>
+                        </div>
+                        <div class="menu-title">Giỏ hàng</div>
+                      </a>
+                    </li>
+                    <li class="menu-label">PAGES</li>
+                    <li>
+                      <a href="#" class="has-arrow">
+                        <div class="child-icon">
+                          <i class="bi bi-caret-right"></i>
+                        </div>
+                        <div class="menu-title">User Profile</div>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" class="has-arrow">
+                        <div class="child-icon">
+                          <i class="bi bi-caret-right"></i>
+                        </div>
+                        <div class="menu-title">FAQ</div>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" class="has-arrow">
+                        <div class="child-icon">
+                          <i class="bi bi-caret-right"></i>
+                        </div>
+                        <div class="menu-title">Timeline</div>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" class="has-arrow">
+                        <div class="child-icon">
+                          <i class="bi bi-caret-right"></i>
+                        </div>
+                        <div class="menu-title">Đăng xuất</div>
+                      </a>
+                    </li>
+                  </ul>
+                  <!-- End navigation -->
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </aside>
