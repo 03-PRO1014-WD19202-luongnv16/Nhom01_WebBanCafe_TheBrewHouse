6675491dc54ee005ccd16cec1b008eee9be8c05f
khanhdao901 <khanhddph47074@fpt.edu.vn>
2024-07-16 12:24:50 +0700

    Delete admin directory

--- admin/component/home.php
+++
@@ -1,9 +0,0 @@
-<main class="main-wrapper">
-      <div class="main-content">
-        <!-- start breadcrumb -->
-        <div class="page-breadcrumb">
-          <div class="breadcrumb-title">Trang chủ</div>
-        </div>
-        <!-- end breadcrumb -->
-      </div>
-</main>
